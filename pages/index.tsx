065dc7fa208b1283287bb1265be4e9c663154559
shota <morgen.ist.auch.noch.ein.tag@gmail.com>
2021-04-29 16:12:52 +0900

    ♻️ TypeScriptに更新

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,31 @@ import Link from 'next/link'
 import Date from '../components/date'
 import styles from '../styles/Home.module.css'
 import { getSortedPostsData } from '../lib/posts'
-import Layout from '../components/layout'
+import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { GetStaticProps } from 'next'
 
-export default function Home(props) {
-  const {allPostsData} = props
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
-      <Head>…</Head>
-      <section className={utilStyles.headingMd}>…</section>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this in{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
@@ -33,7 +49,7 @@ export default function Home(props) {
   )
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
   return {
     props: {
